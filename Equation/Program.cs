35c71c8fc412b63f7fa4fe07353b0ef0babf6669
Виктор Парыгин <paryginva@live.com>
2021-11-30 22:14:24 +0500

    Добавьте файлы проекта.

---
+++ Equation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinEq linear = new LinEq { k = 0.0, b = 0.0 };
+            linear.Root();
+        }
+        struct LinEq
+        {
+            public double k;
+            public double b;
+
+            public void Root()
+            {
+                Console.WriteLine("x = {0}", (-b) / k);
+            }
+        }
+    }
+}
+
---
+++ StructEquation/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructEquation
+{
+    public class LinEq
+    {
+        public double K { get; set; }
+        public double B { get; set; }
+
+        public LinEq(double k, double b)
+        {
+            K = k;
+            B = b;
+        }
+
+        public void Root()
+        {
+            Console.WriteLine("x = {0}", (-B) / K);
+        }
+    }
+}
+
